5c0a1f4c681b2e2f801565f82cf11ed8784dac96
Your Name <you@example.com>
2022-11-06 22:15:30 -0800

    code

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ userinputs=input("All numbers:")
 numbers1=userinputs.split()
 i=0
 x=0
-numbers2=[]
+while x<len(numbers1):
+    temp=numbers1[x]
+    numbers1[x]=int(temp)
+    x=x+1
+numbers2=numbers1.copy()
 while i<len(numbers1):
-    smallest=10000000000000
-    numbers1[i]=int(numbers1[i])
-    while x<len(numbers1):
-        if(smallest>int(numbers1[x])):
-            smallest=numbers1[x]
-        x=x+1
-    numbers2.append(smallest)
-    numbers1.pop(numbers1.index(smallest))
+    minimum=min(numbers2)
+    if minimum<numbers1[i]:
+        numbers1[numbers1.index(minimum)]=numbers1[i]
+        numbers1[i]=minimum
+        numbers2.remove(minimum)
+    print(numbers1)
     i=i+1
-print(numbers2)
-    
